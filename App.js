29c4a20554c4faeb410419e53d8e84334152be40
AniBanani01 <122310408+AniBanani01@users.noreply.github.com>
2023-03-25 20:35:22 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+function App() {
+    return (
+        <>
+            <header className="header">
+                <ul>
+                    <li>
+                        <a href="#home" className="button">HOME</a>
+                    </li>
+
+                    <li>
+                        <a href="#menu" className="button">RECIPIES</a>
+                    </li>
+
+                    <li>
+                        <a href="#about" className="button">ABOUT US</a>
+                    </li>
+
+                    <li>
+                        <a href="#contact" className="button">CONTACT</a>
+                    </li>
+                </ul>
+
+            </header>
+        </>
+    );
+}
+
+export default App;
